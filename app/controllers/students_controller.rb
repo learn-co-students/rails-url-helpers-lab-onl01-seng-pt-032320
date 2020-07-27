9c3c04ccca1b748482c389362f95a2dbedfae99b
nabilhayet <nabil4457@yahoo.com>
2020-07-27 19:56:25 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,7 +6,12 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find_by_id(params[:id])
   end
+
+  def to_s
+    self.first_name + " " + self.last_name 
+  end 
 
   private
 
