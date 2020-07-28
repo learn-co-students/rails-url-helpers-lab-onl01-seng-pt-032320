ff8a760e2d36cbee79a7f23ce4389aac216ea271
jashema_panapa <jcpanapa@gmail.com>
2020-07-28 13:04:35 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
 get "students", to: "students#index"
 
+resources :students, only: :show
+
+get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 
 end
