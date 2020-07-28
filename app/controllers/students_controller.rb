472db3f8c87ec17f09cba7516a6b6f7f18bb7dcd
nabilhayet <nabil4457@yahoo.com>
2020-07-27 23:41:57 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,15 @@ class StudentsController < ApplicationController
     @student = Student.find_by_id(params[:id])
   end
 
+  def activate
+    @student = Student.find_by_id(params[:id])
+      if @student.active == false 
+        @student.active = true
+      else
+        @student.active = false
+      end
+  end 
+
   def to_s
     self.first_name + " " + self.last_name 
   end 
