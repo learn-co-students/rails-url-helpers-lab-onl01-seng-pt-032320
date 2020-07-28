d593344fcf23a45017d961954ee4f75e887460b5
joshpled <joshuapleduc@gmail.com>
2020-07-27 21:01:41 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
 class Student < ActiveRecord::Base
-  def to_s
+  
+  def to_s  
     self.first_name + " " + self.last_name
   end
+
+  def active_status
+    case self.active
+    when true
+      "active"
+    when false
+      "inactive"
+    end
+  end 
 end
